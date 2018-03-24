d7380b0951bae2dce2f1897d9ca407b44d728e7a
QuantumEnergyE <quantumenergye@gmail.com>
2018-03-25 01:58:48 +0800

    detail page

--- product_trace/records/static/js/records.js
+++ product_trace/records/static/js/records.js
@@ -2,7 +2,7 @@ function open_update(uid, url, rid) {
     document.getElementById('update_uid').value = uid;
     document.getElementById('update_url').value = url;
     document.getElementById('update_div').style.display = 'block';
-    document.getElementById('update_form').action = "records/update/" + rid;
+    document.getElementById('update_form').action = "/records/update/" + rid;
 }
 
 function open_create() {
@@ -12,9 +12,11 @@ function open_create() {
 
 function open_del(rid) {
     document.getElementById('del_div').style.display = 'block';
-    document.getElementById('del_form').action = "records/delete/" + rid;
+    document.getElementById('del_form').action = "/records/delete/" + rid;
 }
 
 function search() {
-    uid = document.getElementById('search_input')
+    uid = document.getElementById('search_input').value;
+    url = '/records/detail/' + uid;
+    window.location.href=url;
 }
