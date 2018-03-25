4d903d96c79ac64d6c043634373d161662ca5cfa
QuantumEnergyE <quantumenergye@gmail.com>
2018-03-26 00:28:13 +0800

    static files

--- product_trace/product_trace/settings.py
+++ product_trace/product_trace/settings.py
@@ -117,5 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'common_static'),
+)
 LOGIN_REDIRECT_URL = 'records_list'
 LOGOUT_REDIRECT_URL = '/accounts/login'
