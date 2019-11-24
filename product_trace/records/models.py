17038bae5e02fe02abd6d1033e6178e5db25875e
QuantumEnergyE <quantumenergye@gmail.com>
2019-11-24 19:49:24 +0800

    update

--- product_trace/records/models.py
+++ product_trace/records/models.py
@@ -5,6 +5,6 @@ from django.db import models
 
 class Records(models.Model):
     uid = models.CharField(max_length=256)
-    url = models.URLField()
+    url = models.CharField(max_length=256)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
